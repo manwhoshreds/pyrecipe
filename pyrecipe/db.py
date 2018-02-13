470f6b2149cbfe69de46c786240790ab56310716
Michael Miller <m.k.miller@gmx.com>
2018-02-13 00:41:29 -0600

    new database module

--- pyrecipe/db.py
+++ pyrecipe/db.py
@@ -5,9 +5,18 @@ class RecipeDB:
     """A database subclass for pyrecipe"""
     def __init__(self):
         self.conn = sqlite3.connect(DB_FILE)
-        self.c = self.conn.curser()
+        self.c = self.conn.cursor()
 
-    def exucute(self):
-        pass
+    def execute(self, command):
+        self.c.execute(command)
+
+    def __del__(self):
+        self.conn.close()
+
+    def build_db(self):
+        self.c.execute('''test''')
 
 
+if __name__ == '__main__':
+    db = RecipeDB()
+    db.execute('''hello''')
