86151d797152726888ca3aa4cfab028f726a8825
Michael Miller <m.k.miller@gmx.com>
2018-03-26 00:58:32 -0500

    clean code

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ deps = [
 
 setup(
     name='pyrecipe',
-    version='0.5.1',
+    version='0.5.6',
     packages=['pyrecipe', 'pyrecipe/console_gui', 'pyrecipe/gui'],
     package_data={
         'pyrecipe': ['culinary_units.txt']
