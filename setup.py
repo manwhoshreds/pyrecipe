a3737ffe8fff5010fcef95da89c68da4d21eacd3
Michael Miller <m.k.miller@gmx.com>
2018-03-29 23:03:42 -0500

    edits

--- setup.py
+++ setup.py
@@ -1,21 +1,28 @@
 from distutils.core import setup
 
 deps = [
-        'pint',
-        'urwid',
-        'bs4',
-        'argcomplete',
-        'ruamel.yaml',
-        'inflect',
-        'lxml',
-        'SpeechRecognition',
-        'gtts'
-        ]
+    'pint',
+    'urwid',
+    'bs4',
+    'argcomplete',
+    'ruamel.yaml',
+    'lxml',
+    'SpeechRecognition',
+    'gtts',
+    'termcolor'
+]
+
+packages = [
+    'pyrecipe', 
+    'pyrecipe/console_gui', 
+    'pyrecipe/format',
+    'pyrecipe/db'
+]
 
 setup(
     name='pyrecipe',
     version='0.6.0',
-    packages=['pyrecipe', 'pyrecipe/console_gui'],
+    packages=packages,
     package_data={
         'pyrecipe': ['culinary_units.txt']
         },
